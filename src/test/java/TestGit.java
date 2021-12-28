90b90d0b8a34c48462cc358ee91b916bf94ea780
Verengan122 <96173971+Verengan122@users.noreply.github.com>
2021-12-28 21:38:46 +0300

    Test for GIT

--- src/test/java/TestGit.java
+++ src/test/java/TestGit.java
@@ -13,14 +13,18 @@ public class TestGit {
         Configuration.pageLoadTimeout = 100000;
     }
 
+    String SA = "SoftAssertions";
+    String EW = "@ExtendWith";
+
     @Test
     void fillFromTest() {
-        open("https://github.com/selenide/selenide");
+        open("https://github.com/");
+        $("[name = q]").setValue("Selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
         $("#wiki-tab").click();
         $(".js-wiki-more-pages-link").click();
-        $("[data-filterable-for = wiki-pages-filter]").$(byText("SoftAssertions")).shouldBe(visible).click();
-        //sleep(5000);
-        $$(".highlight-source-java").find(text("@ExtendWith")).shouldBe(visible);
+        $("[data-filterable-for = wiki-pages-filter]").$(byText(SA)).scrollTo().shouldBe(visible).click();
+        $$(".highlight-source-java").find(text(EW)).scrollTo().shouldBe(visible);
 
 
 
